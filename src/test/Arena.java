a7d14f0194240442a57ba4c5ec34420a806535ff
Sergensangue <90864362+Sergensangue@users.noreply.github.com>
2022-02-10 10:31:25 +0100

    Test de la méthode fight et main

--- src/test/Arena.java
+++ src/test/Arena.java
@@ -4,10 +4,10 @@ public class Arena {
         while(!R1.isDead() && !R2.isDead()){
             double random = Math.random()*10;
             if(random > 5){
-                R1.fire(R2); // Tir du robot1 sur le second
+                R1.fire(R2); // Tir du Robot1 sur le second
             }
             else {
-                R2.fire(R1); // Tir du robot 2 sur le premier
+                R2.fire(R1); // Tir du Robot 2 sur le premier
             }
         }
         return R1.isDead() ?  R2 : R1;
